5a8242e7d76c774c6db36921409d844000ad767c
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-03-06 17:27:34 +0100

    Vjezba broj 1

---
+++ Vjezbe/Vjezbe_1/zzadatak_1.2.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+
+while True:
+    try:
+        F = float(input("Unesi silu u N: "))
+        m = float(input("Unesi masu u kg: "))
+
+        if m <= 0:
+            print("Masa ne smije biti manja ili jednaka 0!")
+            continue
+
+        break
+    except:
+        print("Pogrešan unos!")
+
+t_list = []
+x_list = []
+v_list = []
+a_list = []
+
+def f(F, m):
+    a = F/m
+    dt = 0.01
+    t = 0
+    x = 0
+    v = 0
+    while t <= 10:
+        v = v + a*dt
+        x = x + v*dt
+        t_list.append(t)
+        x_list.append(x)
+        v_list.append(v)
+        a_list.append(a)
+        t = t + dt
+
+f(F, m)
+
+def xt_graf (t_list, x_list):
+    plt.plot(t_list, x_list)
+    plt.xlabel("t (s)")
+    plt.ylabel("x (m)")
+    plt.title("x - t graf")
+    plt.show()
+
+def vt_graf (t_list, v_list):
+    plt.plot(t_list, v_list)
+    plt.xlabel("t (s)")
+    plt.ylabel("v (m/s)")
+    plt.title("v - t graf")
+    plt.show()
+
+def at_graf (t_list, a_list):
+    plt.plot(t_list, a_list)
+    plt.xlabel("t (s)")
+    plt.ylabel("a (m/s^2)")
+    plt.title("v - t graf")
+    plt.show()    
+
+xt_graf (t_list, x_list)
+vt_graf (t_list, v_list)
+at_graf (t_list, a_list)
