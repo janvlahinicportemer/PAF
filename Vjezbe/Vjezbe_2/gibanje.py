0ebe45e7f2c88d1cf25efa38902899254d6c2406
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-03-22 09:54:14 +0100

    Vježba broj 2

--- Vjezbe/Vjezbe_2/gibanje.py
+++ Vjezbe/Vjezbe_2/gibanje.py
@@ -1,12 +1,28 @@
 import math
+import numpy as np
 from particle import Particle
 
-imena = []
+V0 = 10
+θ = 45
+X0 = 0
+Y0 = 0
 
-for i in range ():
-    while True:
-        try:
-            v.append(float(input(f"Unesi {k1[i]} koordinatu {k2[i]} točke ({k3[i]}): ")))
-            break
-        except:
-            print ("Pogrešan unos!")
+g = 9.81
+
+p = Particle(V0, θ, X0, Y0)
+
+domet_numerički = p.range()
+
+θ_radian = math.radians(θ)
+domet_analitički = (V0**2 * math.sin(2 * θ_radian)) / g
+
+abs_err  = abs(domet_numerički - domet_analitički)
+rel_err = (abs_err / domet_analitički) * 100
+
+print("Numerički domet:", np.round(domet_numerički, 4), "m")
+print("Analitički domet:", np.round(domet_analitički, 4), "m")
+print("Apsolutna pogreška:", np.round(abs_err, 4), "m")
+print("Relativna pogreška:", np.round(rel_err, 4), "%")
+
+p.plot_trajectory()
+p.reset()
--- Vjezbe/Vjezbe_2/particle.py
+++ Vjezbe/Vjezbe_2/particle.py
@@ -1,8 +1,98 @@
+import math
+import matplotlib.pyplot as plt
+
 class Particle:
     
-    
-    def reset():
-        č
-    
-    def plot_trajectory():
+    def __init__(self, V0, θ, X0, Y0):
+
+        self.V0 = V0
+        self.θ = θ
+        self.X0 = X0
+        self.Y0 = Y0
+
+        self.g = 9.81
+
+        self.X = self.X0
+        self.Y = self.Y0
+
+        θ_radian = math.radians(self.θ)
+
+        self.Vx = self.V0 * math.cos(θ_radian)
+        self.Vy = self.V0 * math.sin(θ_radian)
+
+        self.X_list = [self.X]
+        self.Y_list = [self.Y]
+
+    def reset(self):
         
+        self.V0 = None
+        self.θ = None
+        self.X0 = None
+        self.Y0 = None
+
+        self.X = None
+        self.Y = None
+
+        self.Vx = None
+        self.Vy = None
+        
+        self.X_list = []
+        self.Y_list = []
+
+    def __move(self, dt):
+
+        self.X = self.X + self.Vx * dt
+        self.Y = self.Y + self.Vy * dt
+
+        # Vx = konst.
+        self.Vy = self.Vy - self.g * dt
+
+        if self.Y >= self.Y0:
+            self.X_list.append(self.X)
+            self.Y_list.append(self.Y)
+
+    def range(self):
+
+        dt = 0.01
+
+        self.X = self.X0
+        self.Y = self.Y0
+
+        θ_radian = math.radians(self.θ)
+        self.Vx = self.V0 * math.cos(θ_radian)
+        self.Vy = self.V0 * math.sin(θ_radian)
+
+        self.X_list = [self.X]
+        self.Y_list = [self.Y]
+
+        while self.Y >= self.Y0:
+
+            self.__move(dt)
+
+        return self.X
+
+    def plot_trajectory(self):
+        
+        dt = 0.01
+
+        self.X = self.X0
+        self.Y = self.Y0
+
+        θ_radian = math.radians(self.θ)
+        self.Vx = self.V0 * math.cos(θ_radian)
+        self.Vy = self.V0 * math.sin(θ_radian)
+
+        self.X_list = [self.X]
+        self.Y_list = [self.Y]
+
+        while self.Y >= self.Y0:
+
+            self.__move(dt)
+
+        plt.figure()
+        plt.plot(self.X_list, self.Y_list)
+        plt.xlabel("x (m)")
+        plt.ylabel("y (m)")
+        plt.title("x - y graf")
+        plt.grid(True)
+        plt.show()
