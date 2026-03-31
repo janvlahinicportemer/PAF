8881848215229b55b6bbb4f8a04f87eb8cf834a9
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-03-31 12:48:39 +0200

    Vježba br 3

--- Vjezbe/Vjezbe_3/zadatak_1.py
+++ Vjezbe/Vjezbe_3/zadatak_1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 class Projectile:
     
-    def __init__(self, V0, θ, X0, Y0, m, b, c):
+    def __init__(self, V0, θ, X0, Y0, m, b):
 
         self.V0 = V0
         self.θ = θ
@@ -13,7 +13,6 @@ class Projectile:
         self.g = 9.81
         self.m = m
         self.b = b
-        self.c = c
 
         self.X = self.X0
         self.Y = self.Y0
@@ -23,17 +22,19 @@ class Projectile:
         self.Vx = self.V0 * math.cos(θ_radian)
         self.Vy = self.V0 * math.sin(θ_radian)
 
-        self.a_x = -(self.b*self.Vx + self.c*self.Vx*abs(self.Vx))/self.m
-        self.a_y = -self.g - (self.b*self.Vy + self.c*self.Vy*abs(self.Vy))/self.m
+        v = math.sqrt(self.Vx**2 + self.Vy**2)
+        self.a_x = - (self.b / self.m) * v * self.Vx
+        self.a_y = - self.g - (self.b / self.m) * v * self.Vy 
 
         self.X_list = [self.X]
         self.Y_list = [self.Y]
 
     def __move(self, dt):
 
-        self.a_x = -(self.b*self.Vx + self.c*self.Vx*abs(self.Vx))/self.m
-        self.a_y = -self.g - (self.b*self.Vy + self.c*self.Vy*abs(self.Vy))/self.m
-        
+        v = math.sqrt(self.Vx**2 + self.Vy**2)
+        self.a_x = - (self.b / self.m) * v * self.Vx
+        self.a_y = - self.g - (self.b / self.m) * v * self.Vy 
+
         self.Vx = self.Vx + self.a_x * dt
         self.Vy = self.Vy + self.a_y * dt
 
@@ -42,27 +43,6 @@ class Projectile:
 
         self.X_list.append(self.X)
         self.Y_list.append(self.Y)
-
-    def range(self, dt):
-
-        self.X = self.X0
-        self.Y = self.Y0
-
-        θ_radian = math.radians(self.θ)
-        self.Vx = self.V0 * math.cos(θ_radian)
-        self.Vy = self.V0 * math.sin(θ_radian)
-
-        self.a_x = -(self.b*self.Vx + self.c*self.Vx*abs(self.Vx))/self.m
-        self.a_y = -self.g - (self.b*self.Vy + self.c*self.Vy*abs(self.Vy))/self.m
-        
-        self.X_list = [self.X]
-        self.Y_list = [self.Y]
-
-        while self.Y >= 0:
-
-            self.__move(dt)
-
-        return self.X_list[-2]
 
     def plot_trajectory(self, dt):
 
@@ -73,8 +53,9 @@ class Projectile:
         self.Vx = self.V0 * math.cos(θ_radian)
         self.Vy = self.V0 * math.sin(θ_radian)
 
-        self.a_x = -(self.b*self.Vx + self.c*self.Vx*abs(self.Vx))/self.m
-        self.a_y = -self.g - (self.b*self.Vy + self.c*self.Vy*abs(self.Vy))/self.m
+        v = math.sqrt(self.Vx**2 + self.Vy**2)
+        self.a_x = - (self.b / self.m) * v * self.Vx
+        self.a_y = - self.g - (self.b / self.m) * v * self.Vy 
 
         self.X_list = [self.X]
         self.Y_list = [self.Y]
@@ -93,13 +74,18 @@ class Projectile:
 
 
 dt = float(input("Unesi dt: "))
+
 V0 = float(input("Unesi V0: "))
-0 = float(input("Unesi 0: "))
+θ = float(input("Unesi θ: "))
 X0 = float(input("Unesi X0: "))
 Y0 = float(input("Unesi Y0: "))
 m = float(input("Unesi m: "))
-b = float(input("Unesi b: "))
-c = float(input("Unesi c: "))
 
-p = Projectile(V0, 0, X0, Y0, m, b, c)
+ro = float(input("Unesi ρ: "))
+Cd = float(input("Unesi Cd: "))
+A = float(input("Unesi A: "))
+
+b = (ro * Cd * A) / 2
+
+p = Projectile(V0, θ, X0, Y0, m, b)
 p.plot_trajectory(dt)
