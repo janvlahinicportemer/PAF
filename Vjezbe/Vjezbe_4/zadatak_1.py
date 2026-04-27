ae2a78c6d1302b3145d29521ba08bc34c6dc5656
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-04-27 15:58:53 +0200

    Vježba broj 4

--- Vjezbe/Vjezbe_4/zadatak_1.py
+++ Vjezbe/Vjezbe_4/zadatak_1.py
@@ -1,24 +1,105 @@
-q_e = -1.602e-19
-q_p = +1.602e-19
+import matplotlib.pyplot as plt
 
-m = 9.109e-31
+q_e = -1.602e-19 #(u C)
+q_p = +1.602e-19 #(u c)
 
-x0 = 0
-y0 = 0
-z0 = 0
+m = 9.109e-31 #(u kg)
 
-v_x0 = 1e6
-v_y0 = 0.5e6
-v_z0 = 0.2e6
+x0 = 0 #(u m)
+y0 = 0 #(u m)
+z0 = 0 #(u m)
 
-E_x = 500
-E_y = 300
-E_z = 100
+v_x0 = 1e6 #(u m/s)
+v_y0 = 0.5e6 #(u m/s)
+v_z0 = 0.2e6 #(u m/s)
 
-B_x = 0
-B_y = 0
-B_z = 1e-2 #(u T)
+E_x = [100, 200, 300, 400, 500] #(u V/m)
+E_y = [50, 150, 250, 300, 350] #(u V/m)
+E_z = [0, 50, 100, 150, 200] #(u V/m)
 
-dt = 1e-12
-T = 5e-8
-N = T / dt
+B_x = [0, 0, 0, 0, 0] #(u T)
+B_y = [0, 0, 0, 0, 0] #(u T)
+B_z = [1e-3, 5e-3, 1e-2, 2e-2, 5e-2] #(u T)
+
+dt = 1e-12 #(u s)
+T = 5e-8 #(u s)
+N = int(T / dt)
+
+def gibanje_elektron (q_e, m, x, y, z, v_x, v_y, v_z, E_x, E_y, E_z, B_x, B_y, B_z, dt, N):
+    
+    x_lista = []
+    y_lista = []
+    z_lista = []
+    t_lista = []
+
+    for i in range(N):
+
+        a_x = (q_e/m) * (E_x + v_y*B_z - v_z*B_y)
+        a_y = (q_e/m) * (E_y + v_z*B_x - v_x*B_z)
+        a_z = (q_e/m) * (E_z + v_x*B_y - v_y*B_x)
+
+        v_x = v_x + a_x * dt
+        v_y = v_y + a_y * dt
+        v_z = v_z + a_z * dt
+
+        x = x + v_x * dt
+        y = y + v_y * dt
+        z = z + v_z * dt
+
+        x_lista.append(x)
+        y_lista.append(y)
+        z_lista.append(z)
+        t_lista.append(i*dt)
+    
+    return x_lista, y_lista, z_lista, t_lista
+
+def gibanje_pozitron (q_p, m, x, y, z, v_x, v_y, v_z, E_x, E_y, E_z, B_x, B_y, B_z, dt, N):
+    
+    x_lista = []
+    y_lista = []
+    z_lista = []
+    t_lista = []
+
+    for i in range(N):
+
+        a_x = (q_p/m) * (E_x + v_y*B_z - v_z*B_y)
+        a_y = (q_p/m) * (E_y + v_z*B_x - v_x*B_z)
+        a_z = (q_p/m) * (E_z + v_x*B_y - v_y*B_x)
+
+        v_x = v_x + a_x * dt
+        v_y = v_y + a_y * dt
+        v_z = v_z + a_z * dt
+
+        x = x + v_x * dt
+        y = y + v_y * dt
+        z = z + v_z * dt
+
+        x_lista.append(x)
+        y_lista.append(y)
+        z_lista.append(z)
+        t_lista.append(dt*i)
+
+    return x_lista, y_lista, z_lista, t_lista
+
+#################################################################################################################################################################################################################################################
+
+for i in range (len(B_x)):
+
+    x_lista_e, y_lista_e, z_lista_e, t_lista = gibanje_elektron (q_e, m, x0, y0, z0, v_x0, v_y0, v_z0, E_x[i], E_y[i], E_z[i], B_x[i], B_y[i], B_z[i], dt, N)
+    x_lista_p, y_lista_p, z_lista_p, t_lista = gibanje_pozitron (q_p, m, x0, y0, z0, v_x0, v_y0, v_z0, E_x[i], E_y[i], E_z[i], B_x[i], B_y[i], B_z[i], dt, N)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.plot(x_lista_e, y_lista_e, z_lista_e, label="Elektron")
+    ax.plot(x_lista_p, y_lista_p, z_lista_p, label="Pozitron")
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("z")
+
+    ax.set_title("XYZ graf")
+    ax.legend()
+    plt.show()
+
+#################################################################################################################################################################################################################################################
