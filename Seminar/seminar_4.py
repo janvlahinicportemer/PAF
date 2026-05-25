75c3c6528734d94128529a6cf479d7c7cf686680
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-05-25 14:20:21 +0200

    Seminar

--- Seminar/seminar_4.py
+++ Seminar/seminar_4.py
@@ -3,3 +3,45 @@ from seminar_2 import *
 from seminar_3 import *
 from RK4 import *
 import matplotlib.pyplot as plt
+
+###############################################################################################################################
+
+plt.figure()
+
+plt.plot(M_mnp[:, 0, 1], M_mnp[:, 0, 2])
+plt.scatter(M_mnp[-1, 0, 1], M_mnp[-1, 0, 2], s=50, label="KOMET")
+
+for i, ID in enumerate(ID_valid_list):
+
+    if ID == 10:
+        boja = "yellow"
+        size = 100
+    else:
+        boja = None
+        size = 50
+
+    plt.plot(M_mnp[:, i+1, 1], M_mnp[:, i+1, 2], color=boja)
+    plt.scatter(M_mnp[-1, i+1, 1], M_mnp[-1, i+1, 2], s=size, label=f"{IME_rječnik[ID]}", color=boja)
+
+###############################################################################################################################
+### CENTRIRANJE ###
+x_min = np.min(M_mnp[:, :, 1]) - 1*AU
+x_max = np.max(M_mnp[:, :, 1]) + 1*AU
+
+y_min = np.min(M_mnp[:, :, 2]) - 1*AU
+y_max = np.max(M_mnp[:, :, 2]) + 1*AU
+
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+###############################################################################################################################
+
+plt.xlabel("x [km]")
+plt.ylabel("y [km]")
+plt.title("Putanje kometa i planeta u heliocentričnom sustavu")
+plt.grid(False)
+plt.legend()
+
+plt.savefig("putanje.jpg", dpi=300, bbox_inches="tight")
+
+plt.show()
