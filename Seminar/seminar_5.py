6d0e65e167f0488b708499b71182eb1a2124c700
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-05-27 16:22:14 +0200

    Seminar

--- Seminar/seminar_5.py
+++ Seminar/seminar_5.py
@@ -1,16 +1,15 @@
 from seminar_1 import *
 from seminar_2 import *
 from seminar_3 import *
-import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-
-linije = []
-tocke = []
 
 fig, ax = plt.subplots(figsize=(8, 8))
 
 imena = ["KOMET"] + [IME_rječnik[ID] for ID in ID_valid_list]
+
+linije = []
+tocke = []
 
 for ime in imena:
 
@@ -29,7 +28,7 @@ y_max = np.nanmax(M_mnp[0:i_kraj+1, :, 2]) + AU
 
 ax.set_xlim(x_min, x_max)
 ax.set_ylim(y_min, y_max)
-ax.set_aspect("equal")
+ax.set_aspect("equal", adjustable="box")
 ax.set_xlabel("x [km]")
 ax.set_ylabel("y [km]")
 ax.set_title("Animacija gibanja kometa i planeta u heliocentričnom sustavu")
@@ -39,8 +38,19 @@ ax.legend(loc="upper right")
 rep = 1000
 preskok = 1
 
-def update(frame):
+def init():
+
     for k in range(len(imena)):
+    
+        linije[k].set_data([], [])
+        tocke[k].set_data([], [])
+    
+    return linije + tocke
+
+def update(frame):
+
+    for k in range(len(imena)):
+
         pocetak = max(0, frame - rep)
 
         x = M_mnp[pocetak:frame+1, k, 1]
@@ -51,20 +61,12 @@ def update(frame):
 
     return linije + tocke
 
-ani = FuncAnimation(
-    fig,
-    update,
-    frames=range(1, i_kraj + 1, preskok),
-    interval=1,
-    blit=True,
-    repeat=False
-)
-
-ani.save(
-    "animacija_gibanja.mp4",
-    writer="ffmpeg",
-    fps=60,
-    dpi=200
-)
+ani = FuncAnimation(fig,
+                    update,
+                    init_func=init,
+                    frames=range(1, i_kraj + 1, preskok),
+                    interval=1,
+                    blit=True,
+                    repeat=False)
 
 plt.show()
