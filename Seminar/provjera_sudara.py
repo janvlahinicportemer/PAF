a21daae0f094e1051540bf4ca18049688f4709fb
janvlahinicportemer <jan.vlahinic.portemer@gmail.com>
2026-05-27 14:39:21 +0200

    Seminar

--- Seminar/provjera_sudara.py
+++ Seminar/provjera_sudara.py
@@ -4,6 +4,35 @@ from scipy.optimize import minimize_scalar, brentq
 
 #############################################################################################################################
 
+def S(λ):
+
+    T_λ = Ta + λ * (Tb - Ta)
+
+    h00 = 2*λ**3 - 3*λ**2 + 1
+    h10 = λ**3 - 2*λ**2 + λ
+    h01 = -2*λ**3 + 3*λ**2
+    h11 = λ**3 - λ**2
+
+    Rk_λ = h00*Rka + h10*Δt*Vka + h01*Rkb + h11*Δt*Vkb
+    Rp_λ = h00*Rpa + h10*Δt*Vpa + h01*Rpb + h11*Δt*Vpb
+
+    D_J2000_λ = Rk_λ - Rp_λ
+
+    rotacija = sp.pxform("J2000", body_frame, T_λ)
+    D_planet_λ = rotacija @ D_J2000_λ
+
+    X_λ = D_planet_λ[0]
+    Y_λ = D_planet_λ[1]
+    Z_λ = D_planet_λ[2]
+
+    S_λ = X_λ**2 / A**2 + Y_λ**2 / B**2 + Z_λ**2 / C**2
+
+    return S_λ
+
+#############################################################################################################################
+
+#############################################################################################################################
+
 def provjera_sudara(Ta, Tb, ID,
                     Xka, Yka, Zka,
                     Xkb, Ykb, Zkb,
@@ -30,52 +59,15 @@ def provjera_sudara(Ta, Tb, ID,
 
     R_planet = R_rječnik[ID]
 
-    Rp_x = R_planet[0]
-    Rp_y = R_planet[1]
-    Rp_z = R_planet[2]
+    A = R_planet[0] + R_komet
+    B = R_planet[1] + R_komet
+    C = R_planet[2] + R_komet
 
     body_frame = FRAME_rječnik[ID]
 
     #############################################################################################################################
 
-    def udaljenost_tocka_elipsoid(P, Rp_x, Rp_y, Rp_z):
-
-        X = P[0]
-        Y = P[1]
-        Z = P[2]
-
-        provjera_unutra = X**2/Rp_x**2 + Y**2/Rp_y**2 + Z**2/Rp_z**2
-
-        if provjera_unutra <= 1:
-            return 0
-
-        def F(t):
-            return (
-                (Rp_x**2 * X**2) / (t + Rp_x**2)**2 +
-                (Rp_y**2 * Y**2) / (t + Rp_y**2)**2 +
-                (Rp_z**2 * Z**2) / (t + Rp_z**2)**2
-                - 1
-            )
-
-        t_min = 0
-        t_max = max(Rp_x**2, Rp_y**2, Rp_z**2)
-
-        while F(t_max) > 0:
-            t_max = 2*t_max
-
-        t = brentq(F, t_min, t_max)
-
-        Qx = Rp_x**2 * X / (t + Rp_x**2)
-        Qy = Rp_y**2 * Y / (t + Rp_y**2)
-        Qz = Rp_z**2 * Z / (t + Rp_z**2)
-
-        d = np.sqrt((X - Qx)**2 + (Y - Qy)**2 + (Z - Qz)**2)
-
-        return d
-
-    #############################################################################################################################
-
-    def D(λ):
+    def S(λ):
 
         T_λ = Ta + λ * (Tb - Ta)
 
@@ -92,20 +84,24 @@ def provjera_sudara(Ta, Tb, ID,
         rotacija = sp.pxform("J2000", body_frame, T_λ)
         D_planet_λ = rotacija @ D_J2000_λ
 
-        udaljenost = udaljenost_tocka_elipsoid(D_planet_λ, Rp_x, Rp_y, Rp_z)
+        X_λ = D_planet_λ[0]
+        Y_λ = D_planet_λ[1]
+        Z_λ = D_planet_λ[2]
 
-        return udaljenost
+        S_λ = X_λ**2 / A**2 + Y_λ**2 / B**2 + Z_λ**2 / C**2
+
+        return S_λ
 
     #############################################################################################################################
 
-    rezultat = minimize_scalar(D, bounds=(0, 1), method="bounded")
+    rezultat = minimize_scalar(S, bounds=(0, 1), method="bounded")
 
     λ_min = rezultat.x
-    d_min = rezultat.fun
+    S_min = rezultat.fun
 
     t_sudar = Ta + λ_min * (Tb - Ta)
 
-    if d_min <= R_komet:
+    if S_min <= 1:
         return True, t_sudar
 
     else:
